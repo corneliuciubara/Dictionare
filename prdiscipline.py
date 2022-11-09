b53cc2fd7069b41b31f5750022aa43eb18a9fb95
corneliuciubara <71380231+corneliuciubara@users.noreply.github.com>
2022-11-09 10:58:12 +0200

    Update prdiscipline.py

--- prdiscipline.py
+++ prdiscipline.py
@@ -1,13 +1,13 @@
 disciplina1 = {"Disciplina":"Limba Romana", "Note": [7,8,7,7], "Teza": 8}
 disciplina2 = {"Disciplina":"Matematica", "Note": [7,8,7,10], "Teza": 10}
 disciplina3 = {"Disciplina":"Informatica", "Note": [7,9,10,8], "Teza": 8}
-medialanote1 = sum(disciplina1["Note"]) / 4
+medialanote1 = sum(disciplina1["Note"]) / len(disciplina1["Note"])
 mediapentruan1 = (medialanote1 + int(disciplina1["Teza"])) / 2
 print("La Limba romana, media anuala este de: ", mediapentruan1)
-medialanote2 = sum(disciplina2["Note"]) / 4
+medialanote2 = sum(disciplina2["Note"]) / len(disciplina2["Note"])
 mediapentruan2 = (medialanote2 + int(disciplina2["Teza"])) / 2
 print("La Matematica, media anuala este de: ", mediapentruan2)
-medialanote3 = sum(disciplina3["Note"]) / 4
+medialanote3 = sum(disciplina3["Note"]) / len(disciplina3["Note"])
 mediapentruan3 = (medialanote3 + int(disciplina3["Teza"])) / 2
 print("La Limba romana, media anuala este de: ", mediapentruan3)
 mediatotala = (mediapentruan1 + mediapentruan2 + mediapentruan3) / 3
